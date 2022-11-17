a2bcab0a615b994093e0060ee280848aec37d3e2
Sarath <sarath.regedit@gmail.com>
2022-11-17 21:08:26 +0530

    Variables

---
+++ 02 - Variables/Variables.java
@@ -0,0 +1,11 @@
+class Variables {
+  public static void main(String args[]) {
+    String name = "Bob";
+    int age = 25;
+    float weight = 88.50f;
+    char initial = 'Z';
+    boolean isMale = true;
+
+    System.out.println(name+" "+ initial+", a "+age+" year old man weighing "+weight+"kgs.");
+  }
+}
