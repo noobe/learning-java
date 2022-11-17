585a7a81645fe27e85d1ba842e546bd5d6c2ee2d
Sarath <sarath.regedit@gmail.com>
2022-11-17 21:19:12 +0530

    Switch cases

--- 03 - Conditions/Conditions.java
+++ 03 - Conditions/Conditions.java
@@ -13,5 +13,16 @@ class Conditions {
 
     String res = x > 3 ? "X is greater than 3" : "X is less than 3";
     System.out.println(res);
+
+    switch(x){
+      case 3:
+        System.out.println("X is 3");
+        break;
+      case 4:
+        System.out.println("X is 4");
+        break;
+      default:
+        System.out.println("Value of X is not among the listed options");
+    }
   }
 }
